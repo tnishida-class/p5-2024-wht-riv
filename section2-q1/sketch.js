04045af87f922a0cd399495b2ef833bf9f5ea2cf
wht-riv <2310594h@stu.kobe-u.ac.jp>
2024-11-25 13:13:34 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,15 +1,15 @@
 // 小手調べ
 function setup() {
-  createCanvas(120, 120);
-  background(255);
-  noFill();
+  createCanvas(120, 120);//キャンバス
+  background(255);//背景の色
+  noFill();//塗りつぶしをなくす
   for(let i = 0; i < 10; i++){
-    let d = (i + 1) * 10;
-    if (i < 5) {
-      stroke(0, 0, 255);
+    let d = (i + 1) * 10;//繰り返しを使って円を10回かく
+    if (i < 5) {　//条件分岐
+      stroke(0, 0, 255);　//内側青
     } else {
-      stroke(255, 0, 0);
+      stroke(255, 0, 0);//それ以外赤
     }
-    ellipse(width/2, height/2, d, d);
+    ellipse(width/2, height/2, d, d);//円を描く(x座標、y座標、横幅、縦幅)
   }
 }
