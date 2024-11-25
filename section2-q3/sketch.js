16135ae75241bf3a342cfdfe3c4200df1008c69a
wht-riv <2310594h@stu.kobe-u.ac.jp>
2024-11-25 13:14:18 +0900

    section2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -14,31 +14,32 @@ function setup() {
   strokeWeight(3);
 
  //中心座標と最大半径
-  cx = width / 2; // 中心は (cx, cy)
+  cx = width / 2; // 円の中心を(cx, cy)に設定（キャンバスの中央）
   cy = height / 2;
-  const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
+  const maxR = min(width, height); // 円の最大半径はキャンバスの幅と高さのうち小さい方
  
   //円の描写
-  drawCircle(black, maxR);
-  drawArcs(green, red, maxR * 0.8);
-  drawArcs(black,cream,maxR*0.75);
-  drawArcs(green, red, maxR * 0.5);
-  drawArcs(black,cream,maxR*0.45);
-  drawCircle(green,maxR*0.1);
-  drawCircle(red, maxR * 0.05);
+  drawCircle(black, maxR); // 黒い大きな円
+  drawArcs(green, red, maxR * 0.8); // 緑と赤の扇形
+  drawArcs(black, cream, maxR * 0.75); // 黒とクリームの扇形
+  drawArcs(green, red, maxR * 0.5); // 緑と赤の別の扇形
+  drawArcs(black, cream, maxR * 0.45); // 黒とクリームの扇形
+  drawCircle(green, maxR * 0.1); // 緑色の内側円
+  drawCircle(red, maxR * 0.05); // 中央の赤い円
 }
 
-function drawCircle(c, r){
-  fill(c);
-  ellipse(cx, cy, r, r);
+function drawCircle(c, r){//指定した色ｃと半径rで中心(cx、cy)を基準に円を描写する。
+  fill(c);//図形の塗りつぶしの色
+  ellipse(cx, cy, r, r);//円を描くコード
 }
 
-function drawArcs(c1, c2, r) {
-  for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
+// 円を20個の扇形に分割し、それぞれを交互に色付け
+function drawArcs(c1, c2, r) {//c1,c2と半径ｒで円を描く
+  for (let i = 0; i < 20; i++) {// 20までつづける
+    let start = TWO_PI / 20 * i; //360度を20等分　開始角度
     let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
-    arc(cx, cy, r, r, start, stop, PIE);
+    fill(i % 2 == 0 ? c1 : c2); // 偶数番目をc1、奇数番目をc2で塗る
+    arc(cx, cy, r, r, start, stop, PIE); // 中心 (cx, cy) を基準にして半径 r の扇形を描く
   }
 }
 
